a28c15009f2bca392e8ced7322a1f37378d64324
Leonardo Ribeiro <leolimaribe@gmail.com>
2025-09-09 18:34:48 -0300

    get copilot back to tab

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -5,5 +5,3 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
-vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
-vim.g.copilot_no_tab_map = true
